d3b5652721aa2c8c91a208a5f3826a9762315844
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-09-11 15:48:22 -0600

    Adding new content

---
+++ components/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Banner from './banner'
+
+export default function App() {
+  return (
+    <Banner />
+  )
+}
---
+++ components/banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <header>
+        <div>
+            <img src='./GloboLogo.png' alt='Logo'/>
+        </div>
+        <div>
+            Provinding header text
+        </div>
+    </header>
+  )
+}
