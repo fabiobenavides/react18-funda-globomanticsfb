b476f44d027a371ec9ebf00bcf6891b0df04cf23
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-09-13 15:55:46 -0600

    react and styles

--- components/banner.js
+++ components/banner.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import styles from "./banner.module.css";
 
 export default function Banner() {
   return (
-    <header>
-        <div>
-            <img src='./GloboLogo.png' alt='Logo'/>
+    <header className='row mb-4'>
+        <div className='col-5'>
+            <img src='./GloboLogo.png' alt='Logo' className={styles.logo}/>
         </div>
-        <div>
+        <div className='col-7 mt-5' style={{fontStyle:"italic"}}> /* style is discorage */ 
             Provinding header text
         </div>
     </header>
--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link 
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylescheet" 
+        />
+        <link href="css/globals.css" rel="stylescheet"/>
+      </Head>
       <body>
-        <Main />
+        <div className="container">
+          <Main />
+        </div>
         <NextScript />
       </body>
     </Html>
