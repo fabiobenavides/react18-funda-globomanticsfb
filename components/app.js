66a72befced24bdfe859a8db0829c647e79685e0
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-09-16 15:05:53 -0600

    jsx are objects

--- components/app.js
+++ components/app.js
@@ -2,9 +2,16 @@ import React from 'react'
 import Banner from './banner'
 
 export default function App() {
+
+  const jsx = <div>Hi</div>
+
+
   return (
+    <div>
+      {jsx}
       <Banner>
-        <div>Getting text as props</div>
+        <div>Getting text as props2</div>
       </Banner>
+      </div>
   )
 }
