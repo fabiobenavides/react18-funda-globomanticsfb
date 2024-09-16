6d861a9f90da7dd2b4c6c1b6625b9e167bbab316
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-09-16 15:02:06 -0600

    props destructuring

--- components/banner.js
+++ components/banner.js
@@ -2,7 +2,7 @@ import styles from "./banner.module.css";
 
 //Props are read-only
 
-export default function Banner(props) {
+export default function Banner({headerText}) {
   // style is discorage
   return (
     <header className='row mb-4'>
@@ -10,7 +10,7 @@ export default function Banner(props) {
             <img src='./GloboLogo.png' alt='Logo' className={styles.logo}/>
         </div>
         <div className='col-7 mt-5' style={{fontStyle:"italic"}}> 
-            {props.headerText}
+            {headerText}
         </div>
     </header>
   )
