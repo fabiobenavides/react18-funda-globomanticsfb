98ff04fe19e5b4c60e34733ab5cbce51823b1358
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-09-18 15:16:38 -0600

    React memo example

--- components/HouseRow.js
+++ components/HouseRow.js
@@ -1,7 +1,7 @@
 import currencyFormatter from '@/helpers/currencyFormatter'
 import React from 'react'
 
-export default function HouseRow({ house }) {
+const HouseRow = ({ house }) => {
   return (
     <tr>
         <td>{house.address}</td>
@@ -10,3 +10,9 @@ export default function HouseRow({ house }) {
     </tr>
   )
 }
+
+const HouseRowMem = React.memo(HouseRow); //This will provide cache, but use it with carefully, read doc before using it
+//Where we are using the component:  HouseRow just replace it by: HouseRowMem
+
+export default HouseRow;
+export { HouseRowMem }
