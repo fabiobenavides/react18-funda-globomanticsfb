578208b275714a67b83b20e924f745ef84b3723b
Fabio Benavides <Fabio.Benavides@Henryscheinone.com>
2024-09-20 15:59:41 -0600

    Custom hook

--- hooks/useHouses.js
+++ hooks/useHouses.js
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useHouses = () => {
     const [houses, setHouses] = useState([]);
 
   useEffect(() => {
     const fetchHouses = async () => {
-      const response = await fetchh("/api/houses");
+      const response = await fetch("/api/houses");
       const houses = await response.json();
       setHouses(houses);
     };
     fetchHouses();
   }, []); //The dependency "[]" will fire the effect just once, remove it and it will keep running
-}
 
-export default { useHouses, setHouses };
+  return { houses, setHouses }
+};
+
+export default useHouses;
